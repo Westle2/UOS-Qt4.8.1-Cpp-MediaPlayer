6b5a9d3ef87c8402ba13c9575ca6819273ad48a9
Ther-zh <212880638@qq.com>
2025-03-26 00:17:02 +0800

    添加了逆转情绪以及跟随情绪（一个是逆转情绪，宗旨在于让用户在悲伤等情绪中得到缓解，在兴奋的状态下能够冷静下来做一些事情；一个是跟随情绪，在于让用户沉浸于听歌的情绪中）的model（前面的那个可以保留为”智能推荐”）后续可通过修改传入的path参数来选择对应的模型

--- colorPre/main.py
+++ colorPre/main.py
@@ -2,10 +2,9 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.multioutput import MultiOutputRegressor
 import numpy as np
 import ds
+import reverseDs
+import followDs
 from joblib import dump
-# 模拟训练数据（需替换为真实数据）
-
-# 情绪比例顺序：[中性, 快乐, 悲伤, 愤怒, 恐惧]
 
 
 
@@ -20,7 +19,9 @@ model = MultiOutputRegressor(
     )
 )
 
-model.fit(ds.x_train, ds.y_train)
+#model.fit(ds.x_train, ds.y_train)
+#model.fit(reverseDs.x_train_reverse,reverseDs.y_train_reverse)
+model.fit(followDs.x_train_follow,followDs.y_train_follow)
 to_be_pre=[]
 row0=[]
 for i in range(5):
@@ -32,43 +33,7 @@ predicted_rgb=model.predict(to_be_pre)[0]
 def rgb_to_hex(rgb):
     clamped = np.clip(rgb, 0, 255).astype(int)
     return "#{:02X}{:02X}{:02X}".format(*clamped)
-dump(model,"model.pkl")
-hex_color = rgb_to_hex(predicted_rgb)  # 示例输出：#E3B432
-print(hex_color)
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.multioutput import MultiOutputRegressor
-import numpy as np
-import ds
-from joblib import dump
-# 模拟训练数据（需替换为真实数据）
-
-# 情绪比例顺序：[中性, 快乐, 悲伤, 愤怒, 恐惧]
-
-
-
-# 初始化模型
-
-model = MultiOutputRegressor(
-    RandomForestRegressor(
-        n_estimators=150,
-        max_depth=6,
-        min_samples_split=3,
-        random_state=42
-    )
-)
-
-model.fit(ds.x_train, ds.y_train)
-to_be_pre=[]
-row0=[]
-for i in range(5):
-    row0.append(float(input()))
-to_be_pre.append(row0)
-predicted_rgb=model.predict(to_be_pre)[0]
-# 转换为十六进制
-
-def rgb_to_hex(rgb):
-    clamped = np.clip(rgb, 0, 255).astype(int)
-    return "#{:02X}{:02X}{:02X}".format(*clamped)
-dump(model,"colorPre/model.pkl")
+#dump(model,"reverseModel.pkl")
+dump(model,"followModel.pkl")
 hex_color = rgb_to_hex(predicted_rgb)  # 示例输出：#E3B432
 print(hex_color)
