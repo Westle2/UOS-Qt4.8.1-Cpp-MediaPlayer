5511cebae3d21b21175741f13773dec48f4e59e4
Westle2 <166029372+Westle2@users.noreply.github.com>
2025-05-11 17:46:25 +0800

    新增emo-LLM
    mainwindow-v2里调用的外部文件，当时为了方便打包成安装包，所以没下载过安装包的无法正确读取路径，需手动替换

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 //#include "videoplayer.h"
+#include "chat.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow m;
+    ChatWindow w;
     m.show();
+    w.show();
     return a.exec();
 }
